39b4dd59245713cab93666b8fab148b9d8892a94
root <root@robopar.(none)>
2013-09-27 13:03:45 +0300

    adsf

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-cd /www/$1
-echo '1'
-rm Gemfile.lock
+#rm Gemfile.lock
 bundle install > bundle.txt
-echo 'After bundle'
 RAILS_ENV=production bundle exec rake db:create > db_create.txt
-echo 'after create'
 RAILS_ENV=production bundle exec rake db:migrate > db_migrate.txt
-echo 'after migrate'
 #RAILS_ENV=production bundle exec rake assets:precompile
 #rails g rename:app_to $1 > rename.txt
