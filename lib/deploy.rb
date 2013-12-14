594dfbec49d4f843d2194a55d3a0729719e14c92
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2013-12-14 22:33:09 +0200

    Shift to freeBSD with jails and cool stuff

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -1,54 +1,10 @@
-def set_nginx_config name
-  file = File.open('/opt/nginx/conf/nginx.conf', 'r+') # r+ -> read/write from begginin of a file
-  file.seek(-2, IO::SEEK_END)
-  file.write tenant_config(name)
-  file.close
-end
-
-def tenant_config name
-  ["",
-   "\tserver {",
-   "\t\tlisten 8080;",
-   "\t\tserver_name #{name}.dobri.robopartans.com;",
-   "\t\tpassenger_enabled on;",
-   "\t\troot /var/www-data/#{name}/public;",
-   "\t}",
-   "}"].join "\n"
-end
-
-def database_config database
-  ["production:",
-   "  adapter: mysql2",
-   "  username: root",
-   "  password: robopart",
-   "  database: #{database}",
-   "  pool: 5",
-   "  timeout: 5000"].join "\n"
-end
-
-def create_db
-  `RAILS_ENV=production bundle exec rake db:create > /var/www-data/dbc.log`
-  `RAILS_ENV=production bundle exec rake db:migrate > /var/www-data/dbm.log`
-end
-
-def figaro_configs
-  'SECRET_KEY: ' + `rake secret`
-end
-
-name = ARGV[0]
-
-Dir.chdir('/var/www-data') do
-  `git clone fllcasts #{name}`
-  Dir.chdir("/var/www-data/#{name}") do
-    File.open("/var/www-data/#{name}/config/database.yml", "w") do |file| # w -> overwrite
-      file.write database_config(name)
-    end
-    File.open("/var/www-data/#{name}/config/application.yml", "w") do |file| # w -> overwrite
-      file.write figaro_configs
-    end
-    create_db
-  end
-end
-File.symlink("/var/www-data/#{name}/public", "/var/www-data/projo/public/#{name}")
-set_nginx_config(name)
-`/opt/nginx/sbin/nginx -s reload`
+NAME = ARGV[0]
+`ezjail-admin create #{NAME} #{ENV['server_ip']}`
+location_options = {
+  'passenger_base_uri' => NAME
+  'alias'              => File.join(ENV['jails_dir'], 'root/app/public$1')
+  'passenger_app_root' => File.join(ENV['jails_dir'], 'root/app')
+  'passenger_enabled'  => 'on'
+}
+Nginx::Config.add_location NAME, location_options
+`#{ENV['nginx_bin']} -s reload`
--- lib/nginx.rb
+++ lib/nginx.rb
@@ -1,20 +1,36 @@
 module Nginx
   module Config
+    extend Config # so that methods that use private methods can be called without including the module
     def add_server_block options
       match = File.read(ENV['nginx_config_file']).match(/http\s*{/)
-      config = match.pre_match + match.to_s + server_block(options) + match.post_match
+      import_to_conf match, server_block(options)
+    end
+
+    def add_location path, options # path without /
+      match = File.read(ENV['nginx_config_file']).match(/server\s*{/)
+      import_to_conf match, location_block(path, options)
+    end
+
+    private
+
+    def import_to_conf match, string
+      config = match.pre_match + match.to_s + string + match.post_match
 
       File.open ENV['nginx_config_file'], 'w' do |new_conf|
         new_conf.print config
       end
     end
 
-    private
-
     def server_block options
       "\n\tserver {\n\t\t" +
         options.map { |opt, value| opt.to_s + ': ' + value.to_s + ';' }.join("\n\t\t") +
       "\n\t}\n"
+    end
+
+    def location_block path, options
+      "\n\t\tlocation ~ ^/#{path}(/.*|$) {\n\t\t\t" +
+        options.map { |opt, value| opt.to_s + ': ' + value.to_s + ';' }.join("\n\t\t\t") +
+      "\n\t\t}\n"
     end
   end
 end
