2ecc5509428b03e3afc16d277ceeea4e90819651
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2014-01-04 00:11:43 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrmidon::Application.config.secret_key_base = '958df016a4f8e9b8f7a42a8d9d782022e30dfddef6aaa470be849661b3ac7af4e4facb6dbf0a8dfe15848146b317743aec659c6f8d46600793cb689a86f36790'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myrmidon::Application.config.session_store :cookie_store, key: '_myrmidon_session'
